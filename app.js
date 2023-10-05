3b755614083bc8f7196608a9af6098cd1d5debb3
Andrej <137786054+DujMaa@users.noreply.github.com>
2023-10-05 10:02:08 +0200

    Added new functionalities
    Added new buttons to chose if input would be black or random color.
    Added button to clear grid.
    Small changes to page design

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const grid = document.querySelector(".grid");
+const clearBtn = document.querySelector(".clearBtn");
+const randomColorBtn = document.querySelector(".randomColorBtn");
+const blackColorBtn = document.querySelector(".blackColorBtn");
+let randomColor;
+let isRandomColor = false;
+
+// Function for randomizing color
+
+function randomizeColor() {
+  // color: rgb(red, blue, black)
+  return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+}
+
+//Helper random number function
+function randomNumber() {
+  return Math.floor(Math.random() * 255);
+}
 
 // Function that creates our grid
 
@@ -12,10 +29,32 @@ function makeRows(rows, cols) {
 }
 makeRows(16, 16);
 
+// Painting grid
+
 const gridItems = document.querySelectorAll(".grid-item");
 
 gridItems.forEach((gridItem) => {
   gridItem.addEventListener("mouseleave", (e) => {
-    gridItem.style.backgroundColor = "black";
+    if (isRandomColor) {
+      gridItem.style.backgroundColor = `${randomizeColor()}`;
+    } else {
+      gridItem.style.backgroundColor = "black";
+    }
   });
+});
+
+// Clearing grid
+
+clearBtn.addEventListener("click", (e) => {
+  gridItems.forEach((gridItem) => {
+    gridItem.style.backgroundColor = "white";
+  });
+});
+
+blackColorBtn.addEventListener("click", () => {
+  isRandomColor = false;
+});
+
+randomColorBtn.addEventListener("click", () => {
+  isRandomColor = true;
 });
