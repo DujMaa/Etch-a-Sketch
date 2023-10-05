dfd60cbacf865d3c10f8cca4d0828a8894eb2b18
Andrej <137786054+DujMaa@users.noreply.github.com>
2023-10-05 14:04:59 +0200

    Add grid resize
    Added grid resizeing input

--- app.js
+++ app.js
@@ -1,49 +1,54 @@
 const grid = document.querySelector(".grid");
+
+let gridItems = document.querySelectorAll(".grid-item");
+const gridHeading = document.querySelector(".grid-heading");
+const gridSize = document.querySelector(".gridSize");
+
 const clearBtn = document.querySelector(".clearBtn");
 const randomColorBtn = document.querySelector(".randomColorBtn");
 const blackColorBtn = document.querySelector(".blackColorBtn");
+
 let randomColor;
 let isRandomColor = false;
 
-// Function for randomizing color
-
 function randomizeColor() {
   // color: rgb(red, blue, black)
-  return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+  return `rgb(${randomNumberGenerator()}, ${randomNumberGenerator()}, ${randomNumberGenerator()})`;
 }
 
-//Helper random number function
-function randomNumber() {
+function randomNumberGenerator() {
   return Math.floor(Math.random() * 255);
 }
 
-// Function that creates our grid
-
+// Function that creates grid
 function makeRows(rows, cols) {
+  grid.innerHTML = "";
+  gridHeading.innerText = `Grid Size: ${rows} x ${cols}`;
+
   grid.style.setProperty("--grid-rows", rows);
   grid.style.setProperty("--grid-cols", cols);
+
   for (i = 0; i < rows * cols; i++) {
     let cell = document.createElement("div");
     grid.appendChild(cell).className = "grid-item";
   }
+  gridItems = document.querySelectorAll(".grid-item");
 }
+
 makeRows(16, 16);
+paintGrid();
 
-// Painting grid
-
-const gridItems = document.querySelectorAll(".grid-item");
-
-gridItems.forEach((gridItem) => {
-  gridItem.addEventListener("mouseleave", (e) => {
-    if (isRandomColor) {
-      gridItem.style.backgroundColor = `${randomizeColor()}`;
-    } else {
-      gridItem.style.backgroundColor = "black";
-    }
+function paintGrid() {
+  gridItems.forEach((gridItem) => {
+    gridItem.addEventListener("mouseenter", (e) => {
+      if (isRandomColor) {
+        gridItem.style.backgroundColor = `${randomizeColor()}`;
+      } else {
+        gridItem.style.backgroundColor = "black";
+      }
+    });
   });
-});
-
-// Clearing grid
+}
 
 clearBtn.addEventListener("click", (e) => {
   gridItems.forEach((gridItem) => {
@@ -57,4 +62,9 @@ blackColorBtn.addEventListener("click", () => {
 
 randomColorBtn.addEventListener("click", () => {
   isRandomColor = true;
+});
+
+gridSize.addEventListener("change", (e) => {
+  makeRows(e.target.value, e.target.value);
+  paintGrid();
 });
